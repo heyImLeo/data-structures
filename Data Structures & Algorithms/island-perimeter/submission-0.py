78d046c577208dc121643b26e04825e99612589e
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-10 10:38:05 +0530

    Update: island-perimeter - submission-0

--- Data Structures & Algorithms/island-perimeter/submission-0.py
+++ Data Structures & Algorithms/island-perimeter/submission-0.py
@@ -1,32 +1,26 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
         visit = set()
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
 
-        def dfs(r,c):
-            row_in = 0 <= r < ROWS 
-            col_in = 0 <= c < COLS
-
+        def dfs(r, c, peri):
+            row_in = 0<=r<rows
+            col_in = 0<=c<cols
             if not row_in or not col_in or grid[r][c] == 0:
                 return 1
             
             if (r,c) in visit:
                 return 0
-
-            visit.add((r,c))
-
-            perim = 0
-            for u,v in directions: 
-                newR, newC = r+u, c+v
-                perim += dfs(newR, newC)
             
-            return perim
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c]:
-                    return dfs(r,c)
-        
-        return 0
+            visit.add((r,c))
+            peri = 0
+            for u, v in directions: 
+                peri += dfs(u+r, v+c, peri)
+            return peri
 
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]:
+                    return dfs(r,c, 0)
