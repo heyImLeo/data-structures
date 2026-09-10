36c9ea793d9d1995f76c6cc2633dcfbfe501c577
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-10 13:07:15 +0530

    Update: pacific-atlantic-water-flow - submission-0

--- Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -1,28 +1,25 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
         directions = [(0,1),(1,0),(-1,0),(0,-1)]
         atlantic = set()
         pacific = set()
-        rows, cols = len(heights), len(heights[0])
 
         def dfs(r, c, visit):
             visit.add((r,c))
             for dr, dc in directions:
                 nr, nc = dr+r, dc+c
-                if 0<=nr<rows and 0<=nc<cols and (nr,nc) not in visit and heights[nr][nc] >= heights[r][c]:
-                    visit.add((nr,nc))
+                if 0<=nr<rows and 0<=nc<cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in visit:
                     dfs(nr, nc, visit)
         
         for r in range(rows):
-            dfs(r, 0, pacific) # left column, pacific
-        
+            dfs(r, 0, pacific)
         for r in range(rows):
-            dfs(r, cols-1, atlantic) # right column, atlantic
+            dfs(r, cols-1, atlantic)
 
         for c in range(cols):
-            dfs(0, c, pacific) # top row, pacific
-        
+            dfs(0, c, pacific)
         for c in range(cols):
-            dfs(rows-1, c, atlantic) # bottom row, atlantic
+            dfs(rows-1, c, atlantic)
         
         return list(pacific & atlantic)
