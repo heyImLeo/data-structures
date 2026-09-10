370715f7e534001e619a09ea175cd535d76acfe6
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-10 11:38:05 +0530

    Update: islands-and-treasure - submission-0

--- Data Structures & Algorithms/islands-and-treasure/submission-0.py
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -1,23 +1,18 @@
-from collections import deque
 class Solution:
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
-        
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        queue = deque([(r,c,1) for r in range(rows) for c in range(cols) if grid[r][c] == 0])
         INF = 2147483647
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        queue = deque()
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 0:
-                    queue.append((r, c))
-        
+
         while queue:
-            r, c = queue.popleft()
-            
-            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                nr, nc = r + dr, c + dc
-                
-                if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == INF:
-                    grid[nr][nc] = grid[r][c] + 1
-                    queue.append((nr, nc))
+            row, col, val = queue.popleft()
+
+            for dr, dc in directions:
+                nr, nc = dr+row, dc+col
+
+                if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == INF:
+                    grid[nr][nc] = val
+                    queue.append((nr, nc, val+1))
+
+
