e388bba9f1001866fd8c44941672d6199c30cb3e
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-10 12:18:34 +0530

    Update: surrounded-regions - submission-0

--- Data Structures & Algorithms/surrounded-regions/submission-0.py
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -3,33 +3,31 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        queue = deque([])
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
         rows, cols = len(board), len(board[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
 
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == 'O' and (r==0 or r==rows-1 or c == 0 or c == cols-1):
-                    queue.append((r,c))
-        
-        while queue:
-            r,c = queue.popleft()
-
+        def dfs(r,c):
+            if r not in range(rows) or c not in range(cols) or board[r][c] != 'O':
+                return
             board[r][c] = 'T'
-
-            for dr, dc in directions:
-                nr, nc = dr+r, dc+c
-                if 0<=nr<rows and 0<=nc<cols and board[nr][nc] == 'O':
-                    queue.append((nr,nc))
+            for u, v in directions:
+                nr, nc = r+u, v+c
+                dfs(nr, nc)
+        
+        for r in range(rows):
+            dfs(r, 0)
+        for r in range(rows):
+            dfs(r, cols-1)
+        for c in range(cols):
+            dfs(0, c)
+        for c in range(cols):
+            dfs(rows-1, c)
         
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == 'O':
                     board[r][c] = 'X'
-                elif board[r][c] == 'T':
+                if board[r][c] == 'T':
                     board[r][c] = 'O'
-
-
-
 
         
