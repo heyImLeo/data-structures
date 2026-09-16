bcf2564e7529a09ada7288b99de757063b6c887c
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-16 08:34:51 +0530

    Update: find-target-in-rotated-sorted-array - submission-0

--- Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
@@ -1,20 +1,35 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1
+        def find_min_index():
+            left, right = 0, len(nums)-1
 
-        while left<=right:
-            mid = left + ((right-left)//2)
-
-            if nums[mid] == target:
-                return mid
-            elif nums[left] <= nums[mid]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            else:
-                if nums[mid] <= target <= nums[right]:
+            while left<right:
+                mid = (left+right)//2
+                if nums[mid] >= nums[right]:
                     left = mid+1
                 else:
-                    right = mid-1 
-        return -1
+                    right = mid
+            
+            return left
+        
+        def binary_search(start, end, target):
+            left, right = start, end
+            while left<=right:
+                mid=(left+right)//2
+                if nums[mid] == target:
+                    return mid
+                if nums[mid] < target:
+                    left=mid+1
+                else:
+                    right=mid-1
+            return -1
+
+        
+        min_index = find_min_index()
+        left_search = binary_search(0, min_index-1, target)
+        right_search = binary_search(min_index, len(nums)-1, target)
+        
+        if left_search == -1:
+            return right_search
+        
+        return left_search
