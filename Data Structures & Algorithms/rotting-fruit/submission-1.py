6c37e51e9698e40175cdb4446f4b4f2f5c8dfdfd
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-26 18:19:15 +0530

    Update: rotting-fruit - submission-1

--- Data Structures & Algorithms/rotting-fruit/submission-1.py
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -1,34 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        queue = deque([])
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
-        ROWS = len(grid)
-        COLS = len(grid[0])
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
         fresh = 0
-        minute = 0
+        time = 0
+        queue = deque([])
 
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r,c))
                 if grid[r][c] == 1:
                     fresh+=1
-                elif grid[r][c] == 2:
-                    queue.append((r,c))
         
         while queue and fresh>0:
             length = len(queue)
+
             for _ in range(length):
-                row, col = queue.popleft()
+                r, c = queue.popleft()
 
-                for u,v in directions:
-                    nr, nc = row+u, col+v
-                    if 0<=nr<ROWS and 0<=nc<COLS and grid[nr][nc]==1:
-                        fresh-=1
-                        queue.append((nr,nc))
+                for dr, dc in directions:
+                    nr, nc = dr+r, dc+c
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
-                
+                        queue.append((nr, nc))
+                        fresh-=1
             
-            minute+=1
+            time+=1
         
-        return minute if fresh==0 else -1
-        
-
+        return time if fresh==0 else -1
