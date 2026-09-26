18d061650cb173e633c40574177175ff48fd1957
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-26 18:03:07 +0530

    Update: max-area-of-island - submission-2

--- Data Structures & Algorithms/max-area-of-island/submission-2.py
+++ Data Structures & Algorithms/max-area-of-island/submission-2.py
@@ -1,33 +1,23 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
-        visit = set()
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        maxSize = 0
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        max_size = 0
 
-        def rowColCheck(r,c):
-            row_in = 0<=r<ROWS
-            col_in = 0<=c<COLS
-            return row_in and col_in
-        
-        def dfs(r,c, ROWS, COLS, visit):
-            if not rowColCheck(r,c) or (r,c) in visit or grid[r][c] == 0:
-                return 0
+        def dfs(r, c):
+            cur_size = 1
+            grid[r][c] = 0
 
-            visit.add((r,c))
-
-            size = 1
             for dr, dc in directions:
-                newR, newC = dr + r, dc + c
-                size += dfs(newR, newC, ROWS, COLS, visit)
-            
-            return size
+                nr, nc = dr+r, dc+c
+                if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]:
+                    cur_size+=dfs(nr, nc)
+                    
+            return cur_size
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    size = dfs(r,c, ROWS, COLS, visit)
-                    maxSize = max(maxSize, size)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]:
+                    max_size = max(max_size, dfs(r, c))
         
-        return maxSize
+        return max_size
