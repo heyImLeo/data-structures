14df9307f5b13f69128cd24301f31e30390817a0
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-26 18:55:34 +0530

    Update: surrounded-regions - submission-1

--- Data Structures & Algorithms/surrounded-regions/submission-1.py
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -1,33 +1,23 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        ROWS, COLS = len(board), len(board[0])
+        rows, cols = len(board), len(board[0])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
 
-        def capture(r, c):
-            if (r < 0 or c < 0 or r == ROWS or
-                c == COLS or board[r][c] != "O"
-            ):
-                return
-            board[r][c] = "T"
-            capture(r + 1, c)
-            capture(r - 1, c)
-            capture(r, c + 1)
-            capture(r, c - 1)
-
-        for r in range(ROWS):
-            if board[r][0] == "O":
-                capture(r, 0)
-            if board[r][COLS - 1] == "O":
-                capture(r, COLS - 1)
-
-        for c in range(COLS):
-            if board[0][c] == "O":
-                capture(0, c)
-            if board[ROWS - 1][c] == "O":
-                capture(ROWS - 1, c)
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "T":
-                    board[r][c] = "O"
+        def dfs(r,c):
+            board[r][c] = 'T'
+            for dr, dc in directions:
+                nr, nc = dr+r, dc+c
+                if 0<=nr<rows and 0<=nc<cols and board[nr][nc] == 'O':
+                    dfs(nr, nc)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O' and (r == 0 or r == rows-1 or c == 0 or c == cols-1):
+                    dfs(r,c)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
